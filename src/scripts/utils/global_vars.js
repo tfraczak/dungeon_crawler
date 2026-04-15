75db0b3c123abaaa6ee2226cbf1dc2e32977b36c
Tim Fraczak <tim.fraczak@paynearme.com>
2026-04-15 05:17:33 -0700

    Replace vestigial numeric key codes with string keys in KEYS initialization

--- src/scripts/utils/global_vars.js
+++ src/scripts/utils/global_vars.js
@@ -3,11 +3,12 @@ export const HEIGHT = 720;
 export const SPRITE_DIMS = [48,48];
 export const FPS = 1000/60;
 export const KEYS = {
-  87: false, // W
-  65: false, // A
-  83: false, // S
-  68: false, // D
-  16: false, // L-Shift
+  w: false,
+  a: false,
+  s: false,
+  d: false,
+  Shift: false,
+  Enter: false,
 };
 
 export const SESSION = {};
